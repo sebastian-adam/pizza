634fd937188ddf6c634b90629be8b7c80f961467
Sebastian Adam Prokuski <Subconscious@Sebastians-iMac.local>
2016-06-17 18:46:59 -0700

    add js for receipt backend  and unique frontend output

--- js/scripts.js
+++ js/scripts.js
@@ -2,29 +2,53 @@
 function Pizza() {
   this.diameter;
   this.toppings;
+  this.$diameter;
+  this.$toppings;
   this.total;
 }
 
 Pizza.prototype.chooseSize = function() {
-  this.diameter = parseInt($("select#size").val());
-  if (!this.diameter) {
+  this.diameter = $("#size option:selected").text();
+  this.$diameter = parseInt($("select#size").val());
+  if (!this.$diameter) {
     alert("Please select size.");
     return;
   }
-  return this.diameter;
+  return this.$diameter, this.diameter;
 }
 
 Pizza.prototype.addToppings = function() {
   var priceToppings = 0;
+  var nameToppings = []
   $('input:checkbox:checked').each(function() {
     priceToppings += parseInt($(this).val());
+    nameToppings.push(this.name);
   });
-  return this.toppings = priceToppings;
+  this.$toppings = priceToppings
+  this.toppings = joinAnd(nameToppings);
+  return this.$toppings, this.toppings;
 }
 
 Pizza.prototype.sumTotal = function() {
-  var sum = this.diameter + this.toppings;
+  var sum = this.$diameter + this.$toppings;
   return this.total = "$" + sum + ".00";
+}
+
+Pizza.prototype.submitReceipt1 = function() {
+  return "One " + this.diameter + " " + this.toppings + " pizza will be on it's way soon.";
+}
+
+Pizza.prototype.submitReceipt2 = function() {
+  return "You're total is " + this.total;
+}
+
+function joinAnd(array) {
+  lastItem = array.pop();
+  if (array.length === 0) {
+    return lastItem;
+  } else if (array.length > 0) {
+    return array.join(", ") + " and " + lastItem;
+  }
 }
 
 
@@ -39,6 +63,8 @@ $(function(){
     userPizza.addToppings();
     userPizza.sumTotal();
 
+    $("#receiptTextLine1").text(userPizza.submitReceipt1());
+    $("#receiptTextLine2").text(userPizza.submitReceipt2());
     console.log(userPizza)
     $("#receiptShell").show();
   });
