06a3c356454633c3ac0f29d138ebecf6257eb6ef
Sebastian Adam Prokuski <Subconscious@Sebastians-iMac.local>
2016-06-22 21:48:05 -0700

    refactor prototypes and reorganize frontend and backend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,42 @@
 // BACKEND
-function Pizza() {
-  this.diameter;
-  this.toppings;
-  this.$diameter;
-  this.$toppings;
-  this.total;
+function Pizza(diameter, $diameter, $toppings, toppings, total) {
+  this.diameter = diameter;
+  this.toppings = toppings;
+  this.$diameter = $diameter;
+  this.$toppings = $toppings;
+  this.total = total;
 }
 
-Pizza.prototype.chooseSize = function() {
-  this.diameter = $("#size option:selected").text();
-  this.$diameter = parseInt($("select#size").val());
-  return this.$diameter, this.diameter;
-}
+// Pizza.prototype.chooseSize = function() {
+//   this.diameter = $("#size option:selected").text();
+//   this.$diameter = parseInt($("select#size").val());
+//   return this.$diameter, this.diameter;
+// }
 
-Pizza.prototype.addToppings = function() {
-  var priceToppings = 0;
-  var nameToppings = []
-  $('input:checkbox:checked').each(function() {
-    priceToppings += parseInt($(this).val());
-    nameToppings.push(this.name);
-  });
-  this.$toppings = priceToppings
-  this.toppings = joinAnd(nameToppings);
-  return this.$toppings, this.toppings;
-}
+// Pizza.prototype.addToppings = function() {
+//   var priceToppings = 0;
+//   var nameToppings = []
+//   $('input:checkbox:checked').each(function() {
+//     priceToppings += parseInt($(this).val());
+//     nameToppings.push(this.name);
+//   });
+//   this.$toppings = priceToppings
+//   this.toppings = joinAnd(nameToppings);
+//   return this.$toppings, this.toppings;
+// }
 
-Pizza.prototype.sumTotal = function() {
-  var sum = this.$diameter + this.$toppings;
-  return this.total = "$" + sum + ".00";
-}
+// Pizza.prototype.sumTotal = function() {
+//   var sum = this.$diameter + this.$toppings;
+//   return this.total = "$" + sum + ".00";
+// }
 
-Pizza.prototype.submitReceipt1 = function() {
-  return "One " + this.diameter + " " + this.toppings + " pizza will be on it's way soon.";
-}
+// Pizza.prototype.submitReceipt1 = function() {
+//   return "One " + this.diameter + " " + this.toppings + " pizza will be on it's way soon.";
+// }
 
-Pizza.prototype.submitReceipt2 = function() {
-  return "You're total is " + this.total;
-}
+// Pizza.prototype.submitReceipt2 = function() {
+//   return "You're total is $" + this.total;
+// }
 
 function joinAnd(array) {
   if (!array.length) {
@@ -55,14 +55,26 @@ $(function(){
   $("form#mainForm").submit(function(event){
     event.preventDefault();
 
-    var userPizza = new Pizza();
+    var size = $("#size option:selected").text();
+    var priceSize = parseInt($("select#size").val());
+    var priceToppings = 0;
+    var nameToppings = []
+    $('input:checkbox:checked').each(function() {
+      priceToppings += parseInt($(this).val());
+      nameToppings.push(this.name);
+    });
+    var listToppings = joinAnd(nameToppings);
+    var sum = priceSize + priceToppings;
+    var total = sum.toFixed(2);
 
-    userPizza.chooseSize();
-    userPizza.addToppings();
-    userPizza.sumTotal();
+    var userPizza = new Pizza(size, priceSize, priceToppings, listToppings, total)
 
-    $("#receiptTextLine1").text(userPizza.submitReceipt1());
-    $("#receiptTextLine2").text(userPizza.submitReceipt2());
+    // userPizza.chooseSize();
+    // userPizza.addToppings();
+    // userPizza.sumTotal();
+
+    $("#receiptTextLine1").text("One " + size + " " + listToppings + " pizza will be on it's way soon.");
+    $("#receiptTextLine2").text("You're total is $" + total);
     console.log(userPizza)
     $("#receiptShell").show();
   });
